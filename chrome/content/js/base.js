e6e09357018492188b4a338cf76b955b92f8c697
Maciek Pazurkiewicz <mp248267@students.mimuw.edu.pl>
2012-02-12 19:17:12 +0100

    Added tab reuse (basing on info at https://developer.mozilla.org/en/Code_snippets/Tabbed_browser)

--- chrome/content/js/base.js
+++ chrome/content/js/base.js
@@ -1,12 +1,34 @@
 const OXFORD_DICT_URL = 'http://oald8.oxfordlearnersdictionaries.com/dictionary/'
 
-function openTab(url) {
-    var browser = top.document.getElementById("content");
-    var tab = browser.addTab(url);
-    browser.selectedTab = tab;
-}
-
 function showOALD() {
     url = OXFORD_DICT_URL + content.getSelection();
-    openTab(url);
+    findOrCreateTab('oald', url)
+}
+
+function findTab(marker) {
+    var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+	.getService(Components.interfaces.nsIWindowMediator);
+
+    var tabBrowser = wm.getEnumerator('navigator:browser').getNext().gBrowser;
+    var childNodes = tabBrowser.tabContainer.childNodes
+    for (var index = 0; index < childNodes.length; index++) {
+	var tab = childNodes[index];
+	if (tab['firedict-marker'] == marker) {
+	    return tab;
+	}
+    }
+    return null;
+}
+
+function findOrCreateTab(marker, url) {
+    var tabBrowser = top.document.getElementById("content");
+    tab = findTab(marker);
+
+    if (tab) {
+	tabBrowser.getBrowserForTab(tab).loadURI(url);
+    } else {
+	tab = tabBrowser.addTab(url);
+	tab['firedict-marker'] = marker;
+    }
+    tabBrowser.selectedTab = tab;
 }
